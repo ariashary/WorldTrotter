beeb97ce3f5d14abe7ead94bdd81ff82d05a2502
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-13 14:36:07 +0700

    Initial commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Muhammad Ashary on 13/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
