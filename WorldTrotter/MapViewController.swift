03b94f5edb458d7a412c435e2e94a0dc93413934
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-13 20:35:45 +0700

    Implement programmatic segmented control constraints

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -19,6 +19,21 @@ class MapViewController: UIViewController {
         
         // Set it as *the* view of this view controller
         view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
     }
     
     override func viewDidLoad() {
