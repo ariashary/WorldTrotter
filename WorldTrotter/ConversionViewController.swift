d01e55f63e7e2fff0cf27cdef5f1a4e914c924fd
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-13 15:48:47 +0700

    Implement temperature converter

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -13,11 +13,39 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celciusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
-    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        if let text = textField.text, !text.isEmpty {
-            celciusLabel.text = text
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelciusLabel()
+        }
+    }
+    
+    var celciusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelciusLabel()
+    }
+    
+    func updateCelciusLabel() {
+        if let celciusValue = celciusValue {
+            celciusLabel.text = "\(celciusValue.value)"
         } else {
             celciusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
         }
         
     }
